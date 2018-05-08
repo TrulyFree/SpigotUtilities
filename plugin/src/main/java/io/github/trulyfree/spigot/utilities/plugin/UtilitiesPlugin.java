cf592d9a1f37ea84e74ae21fdb38da13a2a16dd7
Addison Crump <addisoncrump@tamu.edu>
2018-05-07 22:33:58 -0500

    Use smarter way of loading classes.

--- plugin/src/main/java/io/github/trulyfree/spigot/utilities/plugin/UtilitiesPlugin.java
+++ plugin/src/main/java/io/github/trulyfree/spigot/utilities/plugin/UtilitiesPlugin.java
@@ -1,24 +1,59 @@
 package io.github.trulyfree.spigot.utilities.plugin;
 
 import io.github.trulyfree.spigot.utilities.lib.command.TestableCommandExecutor;
+import junit.framework.TestCase;
+import lombok.SneakyThrows;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class UtilitiesPlugin extends JavaPlugin {
-    private static final AtomicBoolean openForSubmission = new AtomicBoolean(true);
     private static final ArrayList<TestableCommandExecutor> utilities = new ArrayList<>();
-
-    public synchronized static boolean submit(TestableCommandExecutor utility) {
-        return openForSubmission.get() && utilities.add(utility);
-    }
 
     @Override
     public void onLoad() {
         super.onLoad();
-        openForSubmission.set(false);
+    }
+
+    @SneakyThrows
+    private void loadClasses() {
+        String pathToJar = this.getClass().getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
+        JarFile jarFile = new JarFile(pathToJar);
+        Enumeration<JarEntry> e = jarFile.entries();
+        URL[] urls = {new URL("jar:file:" + pathToJar + "!/")};
+        URLClassLoader cl = URLClassLoader.newInstance(urls);
+        while (e.hasMoreElements()) {
+            JarEntry je = e.nextElement();
+            if (je.isDirectory() || !je.getName().endsWith(".class")) {
+                continue;
+            }
+            String className = je.getName().substring(
+                    0,
+                    je.getName().length() - 6
+            );
+            className = className.replace(
+                    '/',
+                    '.'
+            );
+            Class<?> clazz = cl.loadClass(className);
+            if (clazz.isAssignableFrom(TestableCommandExecutor.class)) {
+                try {
+                    utilities.add((TestableCommandExecutor) clazz.newInstance());
+                } catch (InstantiationException | IllegalAccessException e1) {
+                    getLogger().warning("Couldn't instantiate utility " + clazz.getSimpleName());
+                    e1.printStackTrace();
+                }
+            }
+        }
     }
 
     @Override
