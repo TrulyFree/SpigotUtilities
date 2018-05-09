a407c8b6ceb2224449589a8cb821f8571d0fb23e
Addison Crump <addisoncrump@tamu.edu>
2018-05-08 22:58:28 -0500

    Fix command executor enabling

--- lib/src/main/java/io/github/trulyfree/spigot/utilities/lib/command/TestableCommandExecutor.java
+++ lib/src/main/java/io/github/trulyfree/spigot/utilities/lib/command/TestableCommandExecutor.java
@@ -4,10 +4,9 @@ import io.github.trulyfree.spigot.utilities.lib.prov.Factory;
 import io.github.trulyfree.spigot.utilities.lib.prov.Provider;
 import lombok.NonNull;
 import org.bukkit.command.CommandExecutor;
-import org.bukkit.event.Listener;
 import org.jetbrains.annotations.Contract;
 
-public interface TestableCommandExecutor extends CommandExecutor, Listener {
+public interface TestableCommandExecutor extends CommandExecutor {
     @Contract("!null, !null -> _")
     void setCommandHandlers(@NonNull Factory<Boolean, CommandExecutionArguments> commandFactory,
                             @NonNull Provider<CommandExecutionArguments> argumentsProvider);
--- misc/ping/src/main/java/io/github/trulyfree/spigot/utilities/misc/ping/PingCommand.java
+++ misc/ping/src/main/java/io/github/trulyfree/spigot/utilities/misc/ping/PingCommand.java
@@ -4,17 +4,13 @@ import io.github.trulyfree.spigot.utilities.lib.Utility;
 import io.github.trulyfree.spigot.utilities.lib.command.standard.StandardCommandExecutionArguments;
 import io.github.trulyfree.spigot.utilities.lib.command.standard.StandardCommandExecutor;
 import lombok.NonNull;
-import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PingCommand extends StandardCommandExecutor implements Utility {
     @Override
     public void onEnable(@NonNull final JavaPlugin plugin) {
         useStandardFactories();
-        plugin.getServer().getPluginManager().registerEvents(
-                this,
-                plugin
-        );
+        plugin.getCommand("ping").setExecutor(this);
     }
 
     @Override
@@ -27,7 +23,6 @@ public class PingCommand extends StandardCommandExecutor implements Utility {
 
     @Override
     public void onDisable() {
-        HandlerList.unregisterAll(this);
     }
 
     @Override
